a0e01bc9a07a64fe6290b2dcd750f33a873998bc
Michael Bergamo <mbergamo@ayrtonsoftware.com>
2019-03-14 11:19:10 -0700

    Added C++ Bubble Sort

---
+++ algorithms/sorting/bubble.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int bubble(int* data, int size) {
+  int swapCount = 0;
+  for (int outer = 0; outer < size; outer++) {
+    for (int inner = 0; inner < size-outer-1; inner++) {
+      if (data[inner] > data[inner+1]) {
+	swapCount++;
+	int temp = data[inner+1];
+	data[inner+1] = data[inner];
+	data[inner] = temp;
+      }
+    }
+  }
+  return swapCount;
+}
+
+void sort(int size) {
+  int* unsortedData = new int[size];
+  int* sortedData = new int[size];
+  for (int idx = 0; idx < size; idx++) {
+    unsortedData[idx] = rand() % 100;
+    sortedData[idx] = unsortedData[idx];
+  }
+  int swapCount = bubble(sortedData, size);
+  cout << "Bubble Sort (Swap Count " << swapCount << ")" << endl;
+  for (int idx = 0; idx < size; idx++) {
+    cout << "data[" << idx << "] = " << unsortedData[idx] << " - " << sortedData[idx] << endl;
+  }
+  delete [] sortedData;
+  delete [] unsortedData;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " <size>" << endl;
+    return 1;
+  }
+  srand (time(NULL));
+  int size = atoi(argv[1]);
+  cout << "Size: " << size << endl;
+  sort(size);
+  return 0;
+}
